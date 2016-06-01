226f3535b07d8eb2d4bfc9ddb667de507523fba8
mengelin <marwwinn@gmail.com>
2016-06-01 15:45:30 +0200

    Changed the width of the 'div3' to 9%

--- RobotHtmlGenerator.java
+++ RobotHtmlGenerator.java
@@ -25,7 +25,7 @@ html()
 			.add(
 			p("Heeeej!")
 			)
-.setId("div1").setStyle(style().backgroundColor("blue").padding("9%"))
+.setId("div1").setStyle(style().backgroundColor("blue").padding("2%"))
 		)
 		.add(
 		div()
@@ -39,7 +39,7 @@ html()
 			.add(
 			p("Tjäna!")
 			)
-.setId("div3")
+.setId("div3").setStyle(style().width("9%"))
 		)
 		.add(
 		div()
