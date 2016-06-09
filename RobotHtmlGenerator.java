5d07a1d8b9cbad7ca276844fe7b2ccb807954082
mengelin <marwwinn@gmail.com>
2016-06-09 11:00:34 +0200

    Changed the color of the 'div1' to orange

--- RobotHtmlGenerator.java
+++ RobotHtmlGenerator.java
@@ -25,7 +25,7 @@ html()
 			.add(
 			p("Heeeej!")
 			)
-.setId("div1").setStyle(style().fontSize("112%"))
+.setId("div1").setStyle(style().fontSize("112%").color("orange"))
 		)
 		.add(
 		div()
@@ -39,7 +39,7 @@ html()
 			.add(
 			p("Tjäna!")
 			)
-.setId("div3")
+.setId("div3").setStyle(style().fontSize("120%"))
 		)
 		.add(
 		div()
